b80031064461f2dfc9e5ba24a64e66f596424d4d
Nerosoft <ggamals7744@yahoo.com>
2017-03-31 11:00:33 +0200

    learn echo in php

---
+++ echo php/echo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+
+
+
+?>
+<html>
+<head>
+<title>Page title</title>
+</head>
+<body>
+The content of the document......
+
+<?php
+
+echo "name abdo";
+echo "<br/>";
+echo"age 22"
+
+
+
+?>
+<?php
+echo "<br/>";
+echo "name abdo";
+
+
+?>
+
+</body>
+
+</html>
+
+<?php
+
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Learn php</title>
+</head>
+<body>
+<h1>All content of the course</h1>
+<h3>
+<ul>
+  <li><a href="echo php/echo.php">Login</a></li>
+  
+</ul>  
+</h3>
+</body>
+
+</html>
